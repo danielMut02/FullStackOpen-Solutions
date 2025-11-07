e283d60baaf0974159a757e1b790b9fdaa3fa0d2
danielMut02 <dmut5502@gmail.com>
2025-11-07 10:51:03 -0500

    Completed exercise 2.4

--- Part 2/courseinfo/src/components/Course.jsx
+++ Part 2/courseinfo/src/components/Course.jsx
@@ -3,37 +3,40 @@ const Header = ({course}) => <h1> {course} </h1>
 const Part = ({part, exercise}) => <p>{part} {exercise}</p>
 
 const Content = ({course}) => {
-    
+    //const content = course.map(course => course.parts)
+    //console.log(content)
+
   return (
     <div>
-    {course.parts.map(parte =>
-        <Part key = {parte.id} part = {parte.name} exercise = {parte.exercises} />
-    )}
+    {course.map(parte =>
+          <Part key = {parte.id} part={parte.name} exercise={parte.exercises} />
+      )
+      }
     </div>
   )
 }
 
 const Total = ({course}) => {
-    const exercisesArray = course.parts.map(array => array.exercises)
+    const exercisesArray = course.map(array => array.exercises)
     const initialValue = 0;
-    const sumWithInitial = exercisesArray.reduce(
+    const sumResult = exercisesArray.reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     initialValue,
 )
 
     return (
-        <p><b>Number of exercises {sumWithInitial}</b></p>
+        <p><b>Number of exercises {sumResult}</b></p>
     )
 
 }
 
 const Course = ({course}) => {
     return (
-    <div key = {course.id}>
+    <>
       <Header course = {course.name} />
-      <Content course = {course} />
-      <Total course = {course} />
-    </div>
+      <Content course = {course.parts} />
+      <Total course = {course.parts} />
+    </>
     )
 }
 
