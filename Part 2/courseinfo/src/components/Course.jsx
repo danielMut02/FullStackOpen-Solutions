7ecd05fa1181d8a712e33ad7ec28661d516b783e
danielMut02 <dmut5502@gmail.com>
2025-11-07 10:05:04 -0500

    Completed exercise 2.3; Used reduce method to calculate total of exercises

--- Part 2/courseinfo/src/components/Course.jsx
+++ Part 2/courseinfo/src/components/Course.jsx
@@ -13,14 +13,26 @@ const Content = ({course}) => {
   )
 }
 
-const Total = (props) => <p><b>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises + props.parts[3].exercises}</b></p>
+const Total = ({course}) => {
+    const exercisesArray = course.parts.map(array => array.exercises)
+    const initialValue = 0;
+    const sumWithInitial = exercisesArray.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue,
+)
+
+    return (
+        <p><b>Number of exercises {sumWithInitial}</b></p>
+    )
+
+}
 
 const Course = ({course}) => {
     return (
     <div key = {course.id}>
       <Header course = {course.name} />
       <Content course = {course} />
-      <Total parts = {course.parts} />
+      <Total course = {course} />
     </div>
     )
 }
