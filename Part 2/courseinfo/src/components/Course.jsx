73b62b74b1cfe91cb56b9e92940ef15fdccea55c
danielMut02 <dmut5502@gmail.com>
2025-11-07 09:18:33 -0500

    Completed exercise 2.1

---
+++ Part 2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,29 @@
+const Header = ({course}) => <h1> {course} </h1>
+
+const Part = ({part, exercise}) => <p>{part} {exercise}</p>
+
+const Content = ({course}) => {
+    
+  return (
+    <div>
+    {course.parts.map(parte =>
+        <Part key = {parte.id} part = {parte.name} exercise = {parte.exercises} />
+    )}
+    </div>
+  )
+}
+
+const Total = (props) => <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises + props.parts[3].exercises}</p>
+
+const Course = ({course}) => {
+    return (
+    <div>
+      <Header course = {course.name} />
+      <Content course = {course} />
+      <Total parts = {course.parts} />
+    </div>
+    )
+}
+
+export default Course
+
