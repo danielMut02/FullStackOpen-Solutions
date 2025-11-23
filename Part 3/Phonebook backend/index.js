e860e450991096869781092ba6b2c749c92bafee
danielMut02 <dmut5502@gmail.com>
2025-11-23 09:49:32 -0500

    Retrieving single contact. Completed exercise 3.3

--- Part 3/Phonebook backend/index.js
+++ Part 3/Phonebook backend/index.js
@@ -40,6 +40,17 @@ app.get('/info', (request, response) => {
   `)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const contact = persons.find(contact => contact.id === id)
+
+    if (contact) {
+        response.json(contact)
+    } else {
+        response.status(404).end()
+    }   
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
